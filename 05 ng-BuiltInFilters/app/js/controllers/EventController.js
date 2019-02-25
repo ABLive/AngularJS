4c5108af150b9d86f44dcc8cc6433a7bb20be74a
test <test@test.com>
2019-02-25 22:51:54 +0530

    Pushing code to git repo

---
+++ 05 ng-BuiltInFilters/app/js/controllers/EventController.js
@@ -0,0 +1,54 @@
+'use strict';
+
+eventsApp.controller('EventController',
+    function EventController($scope) {
+        
+        $scope.sortorder = 'name';
+        $scope.event = {
+            name: 'AngularJS Boot Camp',
+            date: new Date('2016/02/02'),
+            time: '11:00 am',
+            location: {
+                address: 'ABLive Headquarters',
+                city: 'Mumbai',
+                province: 'Maharashtra'
+            },
+            imageUrl: 'img/angularjs-logo.png',
+            sessions: [
+                {
+                    name: 'Directives Masterclass Introductory',
+                    creatorName: 'John Cena',
+                    duration: '1 hr',
+                    level: 'Advanced',
+                    description: 'In this session you will learn how to design and use custom directives. This will help create reusable components',
+                    upVoteCount: 0
+                },
+                {
+                    name: 'Scopes for fun and profit',
+                    creatorName: 'Dwayne Leverock',
+                    duration: '30 mins',
+                    level: 'Introductory',
+                    description: 'This session will take a closer look at scopes and its uses in the AngularJS Application',
+                    upVoteCount: 0
+                },
+                {
+                    name: 'Well Behaved Controllers',
+                    creatorName: 'Phil Brooks',
+                    duration: '2 hours',
+                    level: 'Intermediate',
+                    description: 'Controllers are the beginning of everything AngularJS does require',
+                    upVoteCount: 0
+                }
+            ]
+        }
+    
+        $scope.upVoteSession = function(session) {
+            session.upVoteCount++;
+        };
+        
+        $scope.downVoteSession = function(session) {
+            session.upVoteCount--;
+        };
+    
+    }
+);
